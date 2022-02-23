4bb2a0bbed25fee4285bf2857c7753e88ca71b3e
daria-dudka <d.svirskaya@gmail.com>
2022-02-23 14:08:35 +0200

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const bracketsOpen = [];
+  const bracketsPair = {};
+  let twinsCount = 0;
+
+  bracketsConfig.forEach(item => {
+    bracketsPair[item[1]] = item[0];
+    bracketsOpen.push(item[0]);
+  });
+
+  let bracketsArray = [];
+
+  function isTwinspair(bracket) {
+    return bracketsPair[bracket] === bracket;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+    let lastBracket = bracketsArray[bracketsArray.length - 1];
+
+    if (bracketsOpen.includes(currentBracket)) {
+      if (isTwinspair(currentBracket) && twinsCount === 0) {
+        twinsCount++;
+        bracketsArray.push(currentBracket);
+      } else if (isTwinspair(currentBracket) && twinsCount != 0 && currentBracket === lastBracket) {
+        twinsCount--;
+        bracketsArray.pop();
+      } else if (isTwinspair(currentBracket) && twinsCount != 0 && currentBracket != lastBracket) {
+        twinsCount++;
+        bracketsArray.push(currentBracket);
+      } else {
+        bracketsArray.push(currentBracket);
+      }
+    } else {
+      if (bracketsArray.length === 0) {
+        return false;
+      }
+
+      if (bracketsPair[currentBracket] === lastBracket) {
+        bracketsArray.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return bracketsArray.length === 0;
 }
